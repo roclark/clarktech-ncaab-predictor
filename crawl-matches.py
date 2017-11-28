32588045776601b4537f09c06e66da770e1fb7fe
Robert Clark <robert.d.clark@hpe.com>
2017-11-28 10:16:35 -0600

    Save team stats as CSV files

--- crawl-matches.py
+++ crawl-matches.py
@@ -1,45 +1,50 @@
+import csv
 import os
-import re
 import requests
-
 from bs4 import BeautifulSoup
+from constants import YEAR
 from teams import TEAMS
 
 
-BOXSCORES = 'http://www.sports-reference.com/cbb/boxscores'
-SCHEDULE = 'http://www.sports-reference.com/cbb/schools/%s/2017-schedule.html'
+BOXSCORES = 'http://www.sports-reference.com/cbb/boxscores/'
+SCHEDULE = 'http://www.sports-reference.com/cbb/schools/%s/%s-schedule.html'
 
 
-def create_stats_dict(totals):
-    stats = {}
-    for stat in totals.find_all('td'):
-        field = re.findall('data-stat=".*?"', str(stat))[0]
-        field = re.sub('data-stat=', '', field)
-        field = re.sub('"', '', field)
-        value = stat.get_text()
-        stats[field] = str(value)
-    return stats
+def create_stats_dict(stats_dict, totals, away=False):
+    for tag in totals.find_all('td'):
+        field = str(dict(tag.attrs).get('data-stat'))
+        if away:
+            field = 'opp_%s' % field
+        try:
+            value = float(tag.get_text())
+        except ValueError:
+            # Sometimes, there is no value, such as when a team doesn't attempt
+            # a free throw. In that case, default to 0.
+            value = 0
+        stats_dict[field] = value
+    return stats_dict
 
 
-def find_winner(scores):
+def check_if_home_team_won(scores):
     try:
         away_score = scores[0].get_text()
         home_score = scores[1].get_text()
     except IndexError:
         return None
     if int(away_score) > int(home_score):
-        return 'away'
+        return 0
     else:
-        return 'home'
+        return 1
 
 
-def save_match_stats(match_name, winner, away_stats, home_stats):
+def save_match_stats(match_name, stats):
     match_filename = 'matches/%s' % match_name
+    header = stats.keys()
 
     with open(match_filename, 'w') as match_file:
-        match_file.write('%s\n' % winner)
-        match_file.write('%s\n' % away_stats)
-        match_file.write('%s\n' % home_stats)
+        dict_writer = csv.DictWriter(match_file, header)
+        dict_writer.writeheader()
+        dict_writer.writerows([stats])
 
 
 def match_already_saved(match_name):
@@ -56,14 +61,14 @@ def get_match_data(matches):
             continue
         match_request = requests.get(match)
         match_soup = BeautifulSoup(match_request.text, 'html5lib')
-        winner = find_winner(match_soup.find_all('div', {'class': 'score'}))
+        winner = check_if_home_team_won(match_soup.find_all('div',
+                                                            {'class': 'score'}
+                                                           ))
         team_totals = match_soup.find_all('tfoot')
-        try:
-            away_stats = create_stats_dict(team_totals[0])
-            home_stats = create_stats_dict(team_totals[1])
-        except IndexError:
-            continue
-        save_match_stats(match_name, winner, away_stats, home_stats)
+        stats = create_stats_dict({}, team_totals[0])
+        stats = create_stats_dict(stats, team_totals[1], away=True)
+        stats['home_win'] = winner
+        save_match_stats(match_name, stats)
 
 
 def crawl_team_matches():
@@ -75,7 +80,7 @@ def crawl_team_matches():
         print '[%s/%s] Extracting matches for %s' % (str(count).rjust(3),
                                                      len(TEAMS),
                                                      name)
-        team_schedule = requests.get(SCHEDULE % team)
+        team_schedule = requests.get(SCHEDULE % (team, YEAR))
         team_soup = BeautifulSoup(team_schedule.text, 'html5lib')
         games = team_soup.find_all('table', {'class': 'sortable stats_table'})
 
@@ -87,4 +92,6 @@ def crawl_team_matches():
 
 
 if __name__ == "__main__":
+    if not os.path.exists('matches'):
+        os.makedirs('matches')
     crawl_team_matches()
--- pull-stats.py
+++ pull-stats.py
@@ -1,26 +1,22 @@
+import csv
 import re
 import requests
-
 from bs4 import BeautifulSoup
+from constants import YEAR
 from teams import TEAMS
 
 
-TEAM_PAGE = 'http://www.sports-reference.com/cbb/schools/%s/2017.html'
+TEAM_PAGE = 'http://www.sports-reference.com/cbb/schools/%s/%s.html'
+SCHEDULE_PAGE = 'http://www.sports-reference.com/cbb/schools/%s/%s-schedule.html'
 
 
 def write_team_stats_file(team_link, name, stats):
-    team_stats = {}
-    opponent_stats = {}
-
-    for key, value in stats.items():
-        if key.startswith('opp_'):
-            opponent_stats[key] = value
-        else:
-            team_stats[key] = value
+    header = stats.keys()
 
     with open('team-stats/%s' % team_link, 'w') as team_stats_file:
-        team_stats_file.write('# %s\n' % name)
-        team_stats_file.write('%s\n%s\n' % (team_stats, opponent_stats))
+        dict_writer = csv.DictWriter(team_stats_file, header)
+        dict_writer.writeheader()
+        dict_writer.writerows([stats])
 
 
 def parse_team_stats(team_html):
@@ -29,14 +25,45 @@ def parse_team_stats(team_html):
     for item in team_html.find_all('tr'):
         if 'Opponent' in str(item) or 'Team' in str(item):
             for tag in item.find_all('td'):
-                field = re.findall('data-stat="\S+"', str(tag))[0]
-                field = re.findall('"\S+"', field)[0].replace('"', '')
+                field = str(dict(tag.attrs).get('data-stat'))
                 if 'Opponent' in str(item) and field == 'g':
                     field = 'opp_g'
                 elif 'Opponent' in str(item) and field == 'mp':
                     field = 'opp_mp'
-                team_stats[field] = str(tag.get_text())
+                team_stats[field] = float(tag.get_text())
     return team_stats
+
+
+def include_team_rank(team_stats, ranking):
+    team_stats['rank_1-5'] = 0
+    team_stats['rank_6-10'] = 0
+    team_stats['rank_11-15'] = 0
+    team_stats['rank_16-20'] = 0
+    team_stats['rank_21-25'] = 0
+
+    try:
+        int(ranking)
+    except ValueError:
+        return team_stats
+
+    if ranking < 6:
+        team_stats['rank_1-5'] = 1
+    elif ranking < 11:
+        team_stats['rank_6-10'] = 1
+    elif ranking < 16:
+        team_stats['rank_11-15'] = 1
+    elif ranking < 21:
+        team_stats['rank_16-20'] = 1
+    elif ranking < 26:
+        team_stats['rank_21-25'] = 1
+    return team_stats
+
+
+def parse_team_rank(schedule_html, team_stats):
+    for item in schedule_html.find_all('tr'):
+        if item.find_all('td'):
+            ranking = str(item.find_all('td', class_='center')[-1].get_text())
+            return include_team_rank(team_stats, ranking)
 
 
 def traverse_teams_list():
@@ -47,9 +74,12 @@ def traverse_teams_list():
         print '[%s/%s] Extracting stats for: %s' % (str(teams_parsed).rjust(3),
                                                     len(TEAMS),
                                                     name)
-        team_page = requests.get(TEAM_PAGE % team)
+        team_page = requests.get(TEAM_PAGE % (team, YEAR))
         team_html = BeautifulSoup(team_page.text, 'html5lib')
         team_stats = parse_team_stats(team_html)
+        schedule = requests.get(SCHEDULE_PAGE % (team, YEAR))
+        schedule_html = BeautifulSoup(schedule.text, 'html5lib')
+        team_stats = parse_team_rank(schedule_html, team_stats)
         write_team_stats_file(team, name, team_stats)
 
 
