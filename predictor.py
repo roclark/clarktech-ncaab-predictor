2995efd71017ab27c2e702c766f96b2c4da724fc
Robert Clark <robert.d.clark@hpe.com>
2017-11-28 21:33:00 -0600

    Refactor predictor class to use RandomForestClassifier

--- predictor.py
+++ predictor.py
@@ -1,47 +1,62 @@
-import numpy
-import tensorflow.contrib.learn as skflow
-from sklearn import datasets, metrics
+import pandas as pd
+import numpy as np
 from glob import glob
-from common import create_stats_array, read_match_stats_file, read_team_stats_file
-
-
-AWAY = 0
-HOME = 1
-WINNER = {'away': AWAY,
-          'home': HOME}
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.cross_validation import cross_val_score
 
 
 class Predictor:
     def __init__(self):
         self._classifier = None
-        self._statistics = None
-        self._results = None
+        self._model = None
+        self._targets = None
+        self._test = None
+        self._train = None
 
-        self._read_data()
+        data = self._read_data()
+        self._create_features(data)
         self._create_classifier()
+        self._train_model()
+
+    @property
+    def accuracy(self):
+        xval = cross_val_score(self._classifier,
+                               self._train,
+                               self._targets,
+                               cv = 5,
+                               scoring='accuracy')
+        print 'Accuracy: %s%%' % round(np.mean(xval) * 100.0, 2)
 
     def _read_data(self):
-        self._statistics = []
-        self._results = []
+        data = pd.DataFrame()
 
         for match in glob('matches/*'):
-            winner, away_stats, home_stats = read_match_stats_file(match)
-            self._statistics.append(create_stats_array(eval(away_stats),
-                                                             eval(home_stats)))
-            self._results.append([float(WINNER[winner])])
-        self._statistics = numpy.array(self._statistics)
-        self._target = numpy.array(self._results)
+            data = data.append(pd.read_csv(match))
+        return data
+
+    def _create_features(self, data):
+        train_length = int(len(data) * 0.75)
+        self._targets = data.home_win.head(train_length)
+        train = data.drop('home_win', 1)
+        train.drop('pts', 1, inplace=True)
+        train.drop('opp_pts', 1, inplace=True)
+        self._test = train.iloc[train_length:]
+        self._train = train.head(train_length)
 
     def _create_classifier(self):
-        self._classifier = skflow.TensorFlowLinearClassifier(n_classes=2)
-        self._classifier.fit(self._statistics, self._results)
+        clf = RandomForestClassifier(n_estimators=50, max_features='sqrt')
+        self._classifier = clf.fit(self._train, self._targets)
 
-    def accuracy(self):
-        return metrics.accuracy_score(self._results,
-                                      self._classifier.predict(self._statistics))
+    def _train_model(self):
+        model = SelectFromModel(self._classifier, prefit=True)
+        train_reduced = model.transform(self._train)
 
-    def predict(self, stats):
-        return self._classifier.predict(stats)
-
-    def probability(self, stats):
-        return self._classifier.predict_proba(stats)
+        parameters = {'bootstrap': False,
+                      'min_samples_leaf': 3,
+                      'n_estimators': 50,
+                      'min_samples_split': 10,
+                      'max_features': 'sqrt',
+                      'max_depth': 6}
+        self._model = RandomForestClassifier(**parameters)
+        self._model.fit(train_reduced, self._targets)
