65edab7946dc51b2200c779001fe63bb14e05089
Robert Clark <robert.d.clark@hpe.com>
2017-12-29 14:11:04 -0500

    Split training and testing data with built-in function

--- predictor.py
+++ predictor.py
@@ -5,6 +5,7 @@ from glob import glob
 from sklearn import tree
 from sklearn.externals.six import StringIO
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 
@@ -13,9 +14,10 @@ class Predictor:
     def __init__(self, data_directory='matches'):
         self._classifier = None
         self._model = None
-        self._targets = None
-        self._test = None
-        self._train = None
+        self._X_train = None
+        self._X_test = None
+        self._y_train = None
+        self._y_test = None
 
         data = self._read_data(data_directory)
         self._create_features(data)
@@ -24,8 +26,8 @@ class Predictor:
 
     @property
     def accuracy(self):
-        predicted = self.predict(self._train, int)
-        accuracy = round(accuracy_score(self._targets, predicted) * 100.0, 2)
+        predicted = self.predict(self._X_test, int)
+        accuracy = round(accuracy_score(self._y_test, predicted) * 100.0, 2)
         print 'Accuracy: %s%%' % accuracy
 
     def print_tree(self):
@@ -42,8 +44,9 @@ class Predictor:
             i += 1
 
     def simplify(self, test_data):
-        self._test = test_data.loc[:, test_data.columns.isin(self._filtered_features)]
-        self._test = self._test.reindex(self._filtered_features, axis=1)
+        test_data = test_data.loc[:, test_data.columns.isin(self._filtered_features)]
+        test_data = test_data.reindex(self._filtered_features, axis=1)
+        self._X_test = self._X_test.reindex(self._filtered_features, axis=1)
         parameters = {'bootstrap': False,
                       'min_samples_leaf': 3,
                       'n_estimators': 50,
@@ -51,11 +54,11 @@ class Predictor:
                       'max_features': 'sqrt',
                       'max_depth': 6}
         self._model = RandomForestClassifier(**parameters)
-        self._model.fit(self._train, self._targets)
-        return self._test
+        self._model.fit(self._X_train, self._y_train)
+        return test_data
 
     def predict_probability(self, test_data):
-        return self._reduced_clf.predict_proba(test_data)
+        return self._model.predict_proba(test_data)
 
     def predict(self, test_data, output_datatype):
         return self._model.predict(test_data).astype(output_datatype)
@@ -67,19 +70,18 @@ class Predictor:
         return differential_vector(data)
 
     def _create_features(self, data):
-        self._targets = data.home_win
-        self._train = data.drop('home_win', 1)
+        X = data.drop('home_win', 1)
+        y = data.home_win
+        split_data = train_test_split(X, y)
+        self._X_train, self._X_test, self._y_train, self._y_test = split_data
 
     def _create_classifier(self):
         clf = RandomForestClassifier(n_estimators=50, max_features='sqrt')
-        self._classifier = clf.fit(self._train, self._targets)
+        self._classifier = clf.fit(self._X_train, self._y_train)
 
     def _train_model(self):
-        train = self._train
+        train = self._X_train
         self._model = SelectFromModel(self._classifier, prefit=True)
-        self._train = self._model.transform(self._train)
-        reduced_clf = RandomForestClassifier(n_estimators=50,
-                                             max_features='sqrt')
-        self._reduced_clf = reduced_clf.fit(self._train, self._targets)
+        self._X_train = self._model.transform(self._X_train)
         new_columns = train.columns[self._model.get_support()]
         self._filtered_features = [str(col) for col in new_columns]
