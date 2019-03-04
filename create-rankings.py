peytondodd/basketball-predictor

Update power rankings algorithms
The power rankings algorithm wasn't updated with sportsreference, but
can now be used to generate a giant list of teams in terms of their
relative strength compared to one another.

Signed-Off-By: Robert Clark <robdclark@outlook.com>

create-rankings.py
@@ -2,110 +2,83 @@ import argparse
 import numpy
 import pandas as pd
 import re
-import requests
-from bs4 import BeautifulSoup
-from conferences import CONFERENCES
-from common import differential_vector, find_name_from_nickname
-from constants import YEAR
+from common import (convert_team_totals_to_averages,
+                    differential_vector,
+                    extract_stats_components,
+                    read_team_stats_file)
 from datetime import datetime
 from math import ceil, sqrt
 from predictor import Predictor
-from teams import TEAMS
+from pymongo import MongoClient
+from sportsreference.ncaab.teams import Teams
 
 
 AWAY = 1
 HOME = 0
-TEAM_NAME_REGEX = 'schools/.*?/%s.html' % YEAR
 
 
-def read_team_stats_file(team_filename):
-    return pd.read_csv(team_filename)
-
-
-def convert_team_totals_to_averages(stats):
-    fields_to_average = ['mp', 'fg', 'fga', 'fg2', 'fg2a', 'fg3', 'fg3a', 'ft',
-                         'fta', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov',
-                         'pf']
-    num_games = stats['g']
-    new_stats = stats.copy()
-
-    for field in fields_to_average:
-        new_value = float(stats[field]) / num_games
-        new_stats.loc[:,field] = new_value
-    return new_stats
-
-
-def extract_stats_components(stats, away=False):
-    # Get all of the stats that don't start with 'opp', AKA all of the
-    # stats that are directly related to the indicated team.
-    filtered_columns = [col for col in stats if not str(col).startswith('opp')]
-    stats = stats[filtered_columns]
-    stats = convert_team_totals_to_averages(stats)
-    if away:
-        # Prepend all stats with 'opp_' to signify the away team as such.
-        away_columns = ['opp_%s' % col for col in stats]
-        stats.columns = away_columns
-    return stats
-
-
-def get_totals(games_list, predictions, team_wins, probabilities):
+def get_totals(games_list, predictions, team_mov):
     for i in range(0, len(games_list)):
-        winner = games_list[i][predictions[i]]
-        loser = games_list[i][abs(predictions[i] - 1)]
-        team_wins[winner] += max(probabilities[i])
-        team_wins[loser] += min(probabilities[i])
-    return team_wins
+        winner_index = numpy.argmax(predictions[i])
+        loser_index = abs(winner_index - 1)
+        winner = games_list[i][winner_index]
+        loser = games_list[i][loser_index]
+        margin_of_victory = max(predictions[i]) - min(predictions[i])
+        team_mov[winner] += margin_of_victory
+        team_mov[loser] -= margin_of_victory
+    return team_mov
 
 
 def teams_list(conference):
     if not conference:
-        teams = TEAMS.values()
+        return Teams()
     else:
-        teams = CONFERENCES[conference]
-    return teams
+        teams = []
+        for abbreviation, name in conference['teams'].items():
+            teams.append(abbreviation)
+        return teams
 
 
 def split_datasets(dataset):
-    set_size = int(ceil(sqrt(len(dataset))))
+    set_size = int(ceil(sqrt(len(dataset)))) * 2
     for i in xrange(0, len(dataset), set_size):
         yield dataset[i:i+set_size]
 
 
-def initialize_team_wins(teams):
-    team_wins = {}
+def initialize_team_mov(teams):
+    team_mov = {}
 
     for team in teams:
-        team_wins[team] = 0
-    return team_wins
+        team_mov[team.abbreviation.lower()] = 0
+    return team_mov
 
 
-def update_rankings(rankings, team_wins):
-    sorted_ranks = [(v,k) for k,v in team_wins.iteritems() if v > 0]
+def update_rankings(rankings, team_mov, dataset):
+    sorted_ranks = [(v,k) for k,v in team_mov.iteritems()]
     sorted_ranks.sort(reverse=True)
     for value, team in sorted_ranks:
-        if team not in rankings:
+        if team not in rankings and team in dataset:
             rankings.append(team)
     return rankings
 
 
-def predict_all_matches(predictor, stats_dict, net_ratings, teams):
-    fields_to_rename = {'win_loss_pct': 'win_pct',
-                        'opp_win_loss_pct': 'opp_win_pct'}
-    team_wins = initialize_team_wins(teams)
+def predict_all_matches(predictor, stats_dict, simple_rating_system, teams):
+    team_mov = initialize_team_mov(teams)
     rankings = []
 
-    for dataset in split_datasets(net_ratings):
+    for dataset in split_datasets(simple_rating_system):
         games_list = []
         prediction_stats = pd.DataFrame()
         match_stats = []
         for home_team in dataset:
-            team_wins[home_team] = 0
+            team_mov[home_team] = 0
             print home_team
             for away_team in dataset:
                 if home_team == away_team:
                     continue
-                home_stats = stats_dict[home_team]
-                away_stats = stats_dict['%s_away' % away_team]
+                home_stats = stats_dict[home_team].reset_index(drop=True)
+                away_stats = stats_dict['%s_away' % away_team] \
+                    .reset_index(drop=True)
                 match_stats.append(pd.concat([away_stats, home_stats], axis=1))
                 games_list.append([home_team, away_team])
         try:
@@ -118,33 +91,39 @@ def predict_all_matches(predictor, stats_dict, net_ratings, teams):
             rankings.append(home_team)
             continue
         match_vector = differential_vector(prediction_stats)
-        match_vector.rename(columns=fields_to_rename, inplace=True)
+        match_vector['points_difference'] = match_vector['home_points'] - \
+            match_vector['away_points']
         match_stats_simplified = predictor.simplify(match_vector)
         predictions = predictor.predict(match_stats_simplified, int)
-        probabilities = predictor.predict_probability(match_stats_simplified)
-        team_wins = get_totals(games_list, predictions, team_wins,
-                               probabilities)
-        rankings = update_rankings(rankings, team_wins)
+        team_mov = get_totals(games_list, predictions, team_mov)
+        rankings = update_rankings(rankings, team_mov, dataset)
     return rankings
 
 
 def create_stats_dictionary(teams):
     stats_dict = {}
-    net_rankings = {}
+    simple_rating_system = {}
 
     for team in teams:
-        stats = read_team_stats_file('team-stats/%s' % team)
+        abbreviation = team.abbreviation.lower()
+        stats = read_team_stats_file('team-stats/%s' % abbreviation)
+        if 'defensive_rating' not in stats and \
+           'offensive_rating' in stats and \
+           'net_rating' in stats:
+            stats['defensive_rating'] = stats['offensive_rating'] - \
+                stats['net_rating']
         home_stats = extract_stats_components(stats)
-        net_rankings[team] = float(home_stats.net_rtg)
+        simple_rating_system[abbreviation] = team.simple_rating_system
         away_stats = extract_stats_components(stats, away=True)
-        stats_dict[team] = home_stats
-        stats_dict['%s_away' % team] = away_stats
-    return stats_dict, net_rankings
+        stats_dict[abbreviation] = home_stats
+        stats_dict['%s_away' % abbreviation] = away_stats
+    return stats_dict, simple_rating_system
 
 
-def sort_by_net_rating(net_rankings):
+def sort_by_simple_rating_system(simple_rating_system):
     sorted_ranks = []
-    for key, value in sorted(net_rankings.iteritems(), key=lambda (k,v): (v,k),
+    for key, value in sorted(simple_rating_system.iteritems(),
+                             key=lambda (k,v): (v,k),
                              reverse=True):
         sorted_ranks.append(key)
     return sorted_ranks
@@ -154,9 +133,28 @@ def print_rankings(rankings):
     i = 1
 
     for team in rankings:
-        team = find_name_from_nickname(team)
         print '%s. %s' % (str(i).rjust(3), team)
         i += 1
+
+
+def save_rankings(rankings, skip_mongo, teams_class):
+    if skip_mongo:
+        return
+    overall_rankings = []
+    client = MongoClient()
+    db = client.clarktechsports
+    db.power_rankings.update_many({}, {'$set': {'latest': False}})
+    for rank in rankings:
+        team_details = {
+            rank: {
+                'wins': teams_class(rank).wins,
+                'losses': teams_class(rank).losses,
+                'conference': teams_class(rank).conference
+            }
+        }
+        overall_rankings.append(team_details)
+    db.power_rankings.insert({'latest': True,
+                              'rankings': overall_rankings})
 
 
 def parse_arguments():
@@ -165,18 +163,22 @@ def parse_arguments():
     'conference to analyze the power rankings for. For example, specify "Big '
     'Ten Conference" to get power rankings only comprising the Big Ten teams.',
     default=None)
+    parser.add_argument('--skip-save-to-mongodb', help='Optionally skip saving'
+    ' results to a MongoDB database.', action='store_true')
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
     predictor = Predictor()
-    teams = teams_list(args.conference)
-    stats_dict, net_rankings = create_stats_dictionary(teams)
-    sorted_net_rating = sort_by_net_rating(net_rankings)
-    rankings = predict_all_matches(predictor, stats_dict, sorted_net_rating,
+    teams_class = teams_list(args.conference)
+    teams = [team for team in teams_class]
+    stats_dict, simple_rating_system = create_stats_dictionary(teams)
+    sorted_rating_system = sort_by_simple_rating_system(simple_rating_system)
+    rankings = predict_all_matches(predictor, stats_dict, sorted_rating_system,
                                    teams)
     print_rankings(rankings)
+    save_rankings(rankings, args.skip_save_to_mongodb, teams_class)
 
 
 if __name__ == "__main__":

roclark/clarktech-ncaab-predictor

create-rankings.py
@@ -2,110 +2,78 @@ import argparse
 import numpy
 import pandas as pd
 import re
-import requests
-from bs4 import BeautifulSoup
-from conferences import CONFERENCES
-from common import differential_vector, find_name_from_nickname
-from constants import YEAR
+from common import (differential_vector,
+                    extract_stats_components,
+                    read_team_stats_file)
 from datetime import datetime
 from math import ceil, sqrt
 from predictor import Predictor
-from teams import TEAMS
+from pymongo import MongoClient
+from sportsreference.ncaab.teams import Teams
 
 
-AWAY = 1
-HOME = 0
-TEAM_NAME_REGEX = 'schools/.*?/%s.html' % YEAR
-
-
-def read_team_stats_file(team_filename):
-    return pd.read_csv(team_filename)
-
-
-def convert_team_totals_to_averages(stats):
-    fields_to_average = ['mp', 'fg', 'fga', 'fg2', 'fg2a', 'fg3', 'fg3a', 'ft',
-                         'fta', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov',
-                         'pf']
-    num_games = stats['g']
-    new_stats = stats.copy()
-
-    for field in fields_to_average:
-        new_value = float(stats[field]) / num_games
-        new_stats.loc[:,field] = new_value
-    return new_stats
-
-
-def extract_stats_components(stats, away=False):
-    # Get all of the stats that don't start with 'opp', AKA all of the
-    # stats that are directly related to the indicated team.
-    filtered_columns = [col for col in stats if not str(col).startswith('opp')]
-    stats = stats[filtered_columns]
-    stats = convert_team_totals_to_averages(stats)
-    if away:
-        # Prepend all stats with 'opp_' to signify the away team as such.
-        away_columns = ['opp_%s' % col for col in stats]
-        stats.columns = away_columns
-    return stats
-
-
-def get_totals(games_list, predictions, team_wins, probabilities):
+def get_totals(games_list, predictions, team_mov):
     for i in range(0, len(games_list)):
-        winner = games_list[i][predictions[i]]
-        loser = games_list[i][abs(predictions[i] - 1)]
-        team_wins[winner] += max(probabilities[i])
-        team_wins[loser] += min(probabilities[i])
-    return team_wins
+        winner_index = numpy.argmax(predictions[i])
+        loser_index = abs(winner_index - 1)
+        winner = games_list[i][winner_index]
+        loser = games_list[i][loser_index]
+        margin_of_victory = max(predictions[i]) - min(predictions[i])
+        team_mov[winner] += margin_of_victory
+        team_mov[loser] -= margin_of_victory
+    return team_mov
 
 
 def teams_list(conference):
     if not conference:
-        teams = TEAMS.values()
+        return Teams()
     else:
-        teams = CONFERENCES[conference]
-    return teams
+        teams = []
+        for abbreviation, name in conference['teams'].items():
+            teams.append(abbreviation)
+        return teams
 
 
 def split_datasets(dataset):
-    set_size = int(ceil(sqrt(len(dataset))))
+    set_size = int(ceil(sqrt(len(dataset)))) * 2
     for i in xrange(0, len(dataset), set_size):
         yield dataset[i:i+set_size]
 
 
-def initialize_team_wins(teams):
-    team_wins = {}
+def initialize_team_mov(teams):
+    team_mov = {}
 
     for team in teams:
-        team_wins[team] = 0
-    return team_wins
+        team_mov[team.abbreviation.lower()] = 0
+    return team_mov
 
 
-def update_rankings(rankings, team_wins):
-    sorted_ranks = [(v,k) for k,v in team_wins.iteritems() if v > 0]
+def update_rankings(rankings, team_mov, dataset):
+    sorted_ranks = [(v,k) for k,v in team_mov.iteritems()]
     sorted_ranks.sort(reverse=True)
     for value, team in sorted_ranks:
-        if team not in rankings:
+        if team not in rankings and team in dataset:
             rankings.append(team)
     return rankings
 
 
-def predict_all_matches(predictor, stats_dict, net_ratings, teams):
-    fields_to_rename = {'win_loss_pct': 'win_pct',
-                        'opp_win_loss_pct': 'opp_win_pct'}
-    team_wins = initialize_team_wins(teams)
+def predict_all_matches(predictor, stats_dict, simple_rating_system, teams):
+    team_mov = initialize_team_mov(teams)
     rankings = []
 
-    for dataset in split_datasets(net_ratings):
+    for dataset in split_datasets(simple_rating_system):
         games_list = []
         prediction_stats = pd.DataFrame()
         match_stats = []
         for home_team in dataset:
-            team_wins[home_team] = 0
-            print home_team
+            team_mov[home_team] = 0
+            print(home_team)
             for away_team in dataset:
                 if home_team == away_team:
                     continue
-                home_stats = stats_dict[home_team]
-                away_stats = stats_dict['%s_away' % away_team]
+                home_stats = stats_dict[home_team].reset_index(drop=True)
+                away_stats = stats_dict['%s_away' % away_team] \
+                    .reset_index(drop=True)
                 match_stats.append(pd.concat([away_stats, home_stats], axis=1))
                 games_list.append([home_team, away_team])
         try:
@@ -118,33 +86,39 @@ def predict_all_matches(predictor, stats_dict, net_ratings, teams):
             rankings.append(home_team)
             continue
         match_vector = differential_vector(prediction_stats)
-        match_vector.rename(columns=fields_to_rename, inplace=True)
+        match_vector['points_difference'] = match_vector['home_points'] - \
+            match_vector['away_points']
         match_stats_simplified = predictor.simplify(match_vector)
         predictions = predictor.predict(match_stats_simplified, int)
-        probabilities = predictor.predict_probability(match_stats_simplified)
-        team_wins = get_totals(games_list, predictions, team_wins,
-                               probabilities)
-        rankings = update_rankings(rankings, team_wins)
+        team_mov = get_totals(games_list, predictions, team_mov)
+        rankings = update_rankings(rankings, team_mov, dataset)
     return rankings
 
 
 def create_stats_dictionary(teams):
     stats_dict = {}
-    net_rankings = {}
+    simple_rating_system = {}
 
     for team in teams:
-        stats = read_team_stats_file('team-stats/%s' % team)
+        abbreviation = team.abbreviation.lower()
+        stats = read_team_stats_file('team-stats/%s' % abbreviation)
+        if 'defensive_rating' not in stats and \
+           'offensive_rating' in stats and \
+           'net_rating' in stats:
+            stats['defensive_rating'] = stats['offensive_rating'] - \
+                stats['net_rating']
         home_stats = extract_stats_components(stats)
-        net_rankings[team] = float(home_stats.net_rtg)
+        simple_rating_system[abbreviation] = team.simple_rating_system
         away_stats = extract_stats_components(stats, away=True)
-        stats_dict[team] = home_stats
-        stats_dict['%s_away' % team] = away_stats
-    return stats_dict, net_rankings
+        stats_dict[abbreviation] = home_stats
+        stats_dict['%s_away' % abbreviation] = away_stats
+    return stats_dict, simple_rating_system
 
 
-def sort_by_net_rating(net_rankings):
+def sort_by_simple_rating_system(simple_rating_system):
     sorted_ranks = []
-    for key, value in sorted(net_rankings.iteritems(), key=lambda (k,v): (v,k),
+    for key, value in sorted(simple_rating_system.iteritems(),
+                             key=lambda (k,v): (v,k),
                              reverse=True):
         sorted_ranks.append(key)
     return sorted_ranks
@@ -154,9 +128,28 @@ def print_rankings(rankings):
     i = 1
 
     for team in rankings:
-        team = find_name_from_nickname(team)
         print '%s. %s' % (str(i).rjust(3), team)
         i += 1
+
+
+def save_rankings(rankings, skip_mongo, teams_class):
+    if skip_mongo:
+        return
+    overall_rankings = []
+    client = MongoClient()
+    db = client.clarktechsports
+    db.power_rankings.update_many({}, {'$set': {'latest': False}})
+    for rank in rankings:
+        team_details = {
+            rank: {
+                'wins': teams_class(rank).wins,
+                'losses': teams_class(rank).losses,
+                'conference': teams_class(rank).conference
+            }
+        }
+        overall_rankings.append(team_details)
+    db.power_rankings.insert({'latest': True,
+                              'rankings': overall_rankings})
 
 
 def parse_arguments():
@@ -165,18 +158,22 @@ def parse_arguments():
     'conference to analyze the power rankings for. For example, specify "Big '
     'Ten Conference" to get power rankings only comprising the Big Ten teams.',
     default=None)
+    parser.add_argument('--skip-save-to-mongodb', help='Optionally skip saving'
+    ' results to a MongoDB database.', action='store_true')
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
     predictor = Predictor()
-    teams = teams_list(args.conference)
-    stats_dict, net_rankings = create_stats_dictionary(teams)
-    sorted_net_rating = sort_by_net_rating(net_rankings)
-    rankings = predict_all_matches(predictor, stats_dict, sorted_net_rating,
+    teams_class = teams_list(args.conference)
+    teams = [team for team in teams_class]
+    stats_dict, simple_rating_system = create_stats_dictionary(teams)
+    sorted_rating_system = sort_by_simple_rating_system(simple_rating_system)
+    rankings = predict_all_matches(predictor, stats_dict, sorted_rating_system,
                                    teams)
     print_rankings(rankings)
+    save_rankings(rankings, args.skip_save_to_mongodb, teams_class)
 
 
 if __name__ == "__main__":