fdfc5239fdbb75adc51ba5333e9c7838b643c7c4
Robert Clark <robdclark@outlook.com>
2020-02-05 17:31:16 +0000

    Update

--- predictor.py
+++ predictor.py
@@ -8,12 +8,16 @@ from sklearn import tree
 from sklearn.externals.six import StringIO
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-#from sklearn.ensemble import RandomForestRegressor
-from cuml.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor
+from cuml.ensemble import RandomForestRegressor as curfc
 from sklearn.feature_selection import SelectFromModel
 
 
 DATASET_NAME = 'dataset.pkl'
+HYPER_PARAMETERS = {'bootstrap': False,
+                    'n_estimators': 50,
+                    'max_features': 'sqrt',
+                    'max_depth': 6}
 
 
 class Predictor:
@@ -26,25 +30,17 @@ class Predictor:
         self._y_test = None
 
         data = self._read_data()
-        self._create_features(data)
-        self._create_regressor()
-        self._train_model()
+        self._optimal_features = self._optimize_features(data)
+        optimal_dataset = self._optimize_dataset(data, self._optimal_features)
 
     def simplify(self, test_data):
-        test_data = test_data.loc[:, test_data.columns.isin(self._filtered_features)]
-        test_data = test_data.reindex(self._filtered_features, axis=1)
-        self._X_test = self._X_test.reindex(self._filtered_features, axis=1)
-        parameters = {'bootstrap': False,
-                      'min_samples_leaf': 3,
-                      'n_estimators': 50,
-                      'min_samples_split': 10,
-                      'max_features': 'sqrt',
-                      'max_depth': 6}
-        self._model = RandomForestRegressor(**parameters)
+        data = test_data.loc[:, test_data.columns.isin(self._optimal_features)]
+        self._model = curfc(**HYPER_PARAMETERS)
         self._model.fit(self._X_train, self._y_train)
-        return test_data
+        return data.to_numpy()
 
     def predict(self, test_data, output_datatype):
+        print(self._model)
         return self._model.predict(test_data).astype(output_datatype)
 
     def _read_data(self):
@@ -62,21 +58,34 @@ class Predictor:
         data['points_difference'] = data['home_points'] - data['away_points']
         return differential_vector(data)
 
-    def _create_features(self, data):
-        cudf_data = cudf.DataFrame.from_pandas(data)
-        X = cudf_data.drop('points_difference', 1)
-        y = cudf_data['points_difference']
+    def _optimize_features(self, data):
+        #cudf_data = cudf.DataFrame.from_pandas(data)
+        #X = cudf_data.drop('points_difference', 1)
+        #y = cudf_data['points_difference']
+        X = data.drop('points_difference', 1)
+        y = data['points_difference']
+        X_train, _, y_train, _ = train_test_split(X, y)
+        regressor = self._create_regressor(X_train, y_train)
+        features = self._select_features(regressor, X_train)
+        return features
+
+    def _create_regressor(self, X_train, y_train):
+        reg = RandomForestRegressor(**HYPER_PARAMETERS)
+        regressor = reg.fit(X_train, y_train)
+        return regressor
+
+    def _select_features(self, regressor, X_train):
+        train = X_train
+        model = SelectFromModel(regressor, prefit=True, threshold=0.01)
+        X_train = model.transform(X_train)
+        new_columns = train.columns[model.get_support()]
+        return [str(col) for col in new_columns]
+
+    def _optimize_dataset(self, data, optimal_features):
+        #cudf_data = cudf.DataFrame.from_pandas(data)
+        #X = data.drop('points_difference', 1)
+        X = data.loc[:, optimal_features].to_numpy()
+        #y = data['points_difference']
+        y = data['points_difference'].to_numpy()
         split_data = train_test_split(X, y)
         self._X_train, self._X_test, self._y_train, self._y_test = split_data
-
-    def _create_regressor(self):
-        reg = RandomForestRegressor(n_estimators=50, max_features='sqrt')
-        self._regressor = reg.fit(self._X_train, self._y_train)
-
-    def _train_model(self):
-        train = self._X_train
-        self._model = SelectFromModel(self._regressor, prefit=True,
-                                      threshold=0.01)
-        self._X_train = self._model.transform(self._X_train)
-        new_columns = train.columns[self._model.get_support()]
-        self._filtered_features = [str(col) for col in new_columns]
